0dcb0965aa5b7d83ac3cec91898f1cc8e3800b69
Jean Yves Gatwaza <104692473+jGatwaza@users.noreply.github.com>
2024-06-06 17:18:08 +0200

    Adding redux

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
-
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../components/Navbar";
 import Heading from "../../components/Heading";
 import SubHeading from "../../components/Subheading";
 import BlogGrid from "../../components/BlogGrid";
 import CategoriesList from "../../components/CategoriesList";
 import Footer from "../../components/Footer";
-
+import { fetchBlogs, reset as resetBlogs } from "../../features/blogsSlice";
 import blogService from "../../services/blogService";
 import categoryService from "../../services/categoryService";
 
 export default function Home() {
   const [blogs, setBlogs] = useState();
   const [categories, setCategories] = useState();
-
+  const dispatch = useDispatch();
+  
   useEffect(() => {
-    const fetchBlogs = async () => {
+    const fetchData = async () => {
       try {
         const blogsRes = await blogService.fetchBlogs();
         const categoryRes = await categoryService.fetchCategories();
@@ -25,7 +26,7 @@ export default function Home() {
         console.log(err);
       }
     };
-    fetchBlogs();
+    fetchData();
   }, []);
 
   return (
